ab8932cce8509c94c0f3449ee585500e887254fe
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-23 13:34:53 +0100

    Uitwerking van de exercise

---
+++ tijd.php
@@ -0,0 +1,11 @@
+<?php
+$imput = $argv[1];
+if (strpos($imput, "s")){
+    $sec = (int)$imput;
+}
+else{
+    echo "geen seconden gezien" . PHP_EOL;
+    exit;
+}
+echo "$sec seconden" . PHP_EOL;
+?>
